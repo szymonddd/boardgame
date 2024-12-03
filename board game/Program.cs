ba2967b1d80e57576b779ac615447488394f40b6
szymond <memememe@gmail.com>
2024-12-03 09:26:27 +0100

    dodanie klasy player i board

---
+++ board game/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Player
+{
+    public string Name { get; set; }
+    public int Position { get; set; }
+    public int Score { get; set; }
+}
+
+class Board
+{
+    public int Size { get; set; }
+    private int[] rewards;
+    
+}
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
